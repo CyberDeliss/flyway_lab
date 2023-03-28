cee341a67dcb8a8a2d02cbb505aca2327e5cdc57
CyberDeliss <ganyajob@gmail.com>
2023-03-28 15:10:38 +0400

    create R migration v_employees

---
+++ db/migrations/views/R__v_employees.sql
@@ -0,0 +1,15 @@
+create view V_EMPLOYEES(
+    employee_name,
+    job_name,
+    dep_name
+    )AS select distinct
+        e.employee_name,
+        j.job_name,
+        d.dep_name
+
+        from AGAFIA_TEMEROVA.EMPLOYEES e
+        join AGAFIA_TEMEROVA.DEPARTMENTS d
+          on e.dep_id = d.dep_id
+        join AGAFIA_TEMEROVA.JOBS j
+          on e.job_id = j.job_id
+;
